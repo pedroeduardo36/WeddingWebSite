ebbef63f711e97d02f5e7d0ee179d9b51b31974b
pedroeduardo36 <pedrovilarinho.gpd@gmail.com>
2026-01-18 15:40:57 -0300

    Fix Gift import erro

--- src/components/sections/GiftListSection.tsx
+++ src/components/sections/GiftListSection.tsx
@@ -2,7 +2,8 @@
 
 import { useState, useEffect } from "react";
 import Link from "next/link";
-import GiftCard, { Gift } from "@/components/GiftCard";
+import GiftCard from "@/components/GiftCard";
+import type { Gift } from "@/lib/gifts";
 import { Separator } from "@/components/ui/separator";
 import { Button } from "@/components/ui/button";
 import { allGifts } from "@/lib/gifts";
