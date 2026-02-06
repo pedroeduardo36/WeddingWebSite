78fea1564f2a4e2137b6eeb06aed410c3ef84907
JuliaNazka <nazkajulia@gmail.com>
2026-02-06 12:06:48 -0300

    Correção texto Nossa historia

--- src/components/sections/OurStorySection.tsx
+++ src/components/sections/OurStorySection.tsx
@@ -73,7 +73,7 @@ export default function OurStorySection() {
                 Nossa história começou como as melhores costumam começar: sem pressa, dentro do nosso PG. Ali, enquanto buscávamos a Deus, acabamos encontrando também um ao outro. O namoro em 2023 foi só a confirmação de que nossos propósitos estavam alinhados e de que amar é, acima de tudo, uma escolha diária de cuidar e crescer junto.
                 </p>
                 <p className="text-muted-foreground text-justify leading-relaxed">
-                Em janeiro de 2025, no Rio de Janeiro, decidimos que queriamos essa parceriapara a vida toda. O "sim" do noivado foi o primeiro passo para o grande dia que vem por aí. Estamos ansiosos para celebrar com vocês o inicio da nossa família, com a ceteza que Jesus estará guiando cada passo e sendo o dono da trilha sonora da nossa vida!
+                Em janeiro de 2025, no Rio de Janeiro, decidimos que queriamos essa parceriapara a vida toda. O "sim" do noivado foi o primeiro passo para o grande dia que vem por aí. Estamos ansiosos para celebrar com vocês o inicio da nossa família, com a certeza que Jesus estará guiando cada passo e sendo o dono da trilha sonora da nossa vida!
               </p>  
               </div>
           
